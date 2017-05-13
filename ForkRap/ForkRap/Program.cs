1c581f6c9f02dc756a120b69e06a665784d18b34
Anwar Faiz <mfaiz@expedia.com>
2017-05-13 15:13:51 +0530

    making C# console project structure and provisioning for accepting commandline parameters

---
+++ ForkRap/ForkRap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkRap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Error: You have not provided any parameter!");
+                System.Console.WriteLine("Syntax: forkwrap <url> <number_of_times> <destination_folder_path>");
+                return;
+            }
+            if (args[0] == "--help" || args[0] == "-h")
+            {
+                System.Console.WriteLine("ForkRap: [Help]");
+                System.Console.WriteLine("Syntax: forkwrap <url> <number_of_times> <destination_folder_path>");
+                System.Console.WriteLine("More details to be added ...");
+            }
+
+
+
+            // Stopping console to read result
+            Console.ReadKey();
+        }
+    }
+}
