a6c953cfe2ae3b813d51892d04333922d2ae8bd5
Anwar Faiz <mfaiz@expedia.com>
2017-05-13 23:02:32 +0530

    Added the validations on inputs

---
+++ ForkRap/ForkRap/SafeFileIO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkRap
+{
+    class SafeFileIO
+    {
+        public static void IfPathNotExistsCreateIt(string path)
+        {
+            bool exists = System.IO.Directory.Exists(path);
+
+            if (!exists)
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                catch( Exception e)
+                {
+                    System.Console.WriteLine("Error Creating directory!");
+                    System.Console.WriteLine("Error Stacktrace: \n" + e.ToString());
+                }
+            }
+                
+        }
+    }
+}
